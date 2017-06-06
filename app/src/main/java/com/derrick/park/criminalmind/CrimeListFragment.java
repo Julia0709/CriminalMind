75da466158e05bd071498392386b2ca73a154265
Julia <berrylily.0709@gmail.com>
2017-06-06 05:55:16 -0700

    Display number in itemView

--- app/src/main/java/com/derrick/park/criminalmind/CrimeListFragment.java
+++ app/src/main/java/com/derrick/park/criminalmind/CrimeListFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,6 +20,7 @@ public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdater;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
@@ -35,9 +38,32 @@ public class CrimeListFragment extends Fragment {
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+
         }
+
+        public void bind(final Crime crime) {
+
+            mTitleTextView.setText(crime.getTitle());
+            mDateTextView.setText(crime.getDate().toString());
+
+            // toast when itemView is clicked
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "Number: " + crime.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
@@ -53,9 +79,9 @@ public class CrimeListFragment extends Fragment {
             return new CrimeHolder(layoutInflater, parent);
         }
 
-        @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
-            // assignment
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
         }
 
         @Override
